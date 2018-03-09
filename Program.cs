3b8539a904f4cc4baacbc2b5165b327e0a81db43
Rumaruka <rumarukakunnya@gmail.com>
2018-03-09 20:26:10 +0500

    Init new version bots

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Initilization API
+using TwitchLib.Models.Client;
+using TwitchLib.Events.Client;
+using TwitchLib.Models.API.v5.UploadVideo;
+using TwitchChatBot;
+
+namespace TwitchChatBotRMK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Запуск Twitch bot
+            */
+            TwitchChatBot bot = new TwitchChatBot();
+            bot.Connect();
+            Console.ReadLine();
+       
+            bot.Disconnect();
+            Console.ReadLine();
+            Console.Clear();
+            
+        }
+    }
+}
---
+++ TwitchAPIChat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib;
+using TwitchLib.Api.Sections;
+
+namespace TwitchChatBot
+{
+    class TwitchAPIChat
+    {
+        public Undocumented Undocumented { get; }
+        public Users Users { get; }
+        public Videos Videos { get; }
+        public Debugging Debugging { get; }
+        public Teams Teams { get; }
+        public Subscriptions Subscriptions { get; }
+        public Streams Streams { get; }
+        public Search Search { get; }
+        public Root Root { get; }
+        public Ingests Ingests { get; }
+        public Games Games { get; }
+        public Follows Follows { get; }
+        public Communities Communities { get; }
+        public Collections Collections { get; }
+        public Clips Clips { get; }
+        public Chat Chat { get; }
+        public Channels Channels { get; }
+        public ChannelFeeds ChannelFeeds { get; }
+        public Bits Bits { get; }
+        public Badges Badges { get; }
+        public Blocks Blocks { get; }
+        public Auth Auth { get; }
+        public IApiSettings Settings { get; }
+        public ThirdParty ThirdParty { get; }
+        public Webhooks Webhooks { get; }
+    }
+}
